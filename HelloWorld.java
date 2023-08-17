563ed030e3dc48d52367bde79bae2db641bd2164
naveen <naveenappasani7@gmail.com>
2023-08-17 07:12:31 +0000

    Added 2lines Git

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,7 @@ public class HelloWorld {
     System.out.println("Hello world!");
     System.out.println("Hello Devops!");
     System.out.println("Hello Linux!");
+    System.out.println("Hi Buddy!");
+    System.out.println("Hello Github!);
   }
 }
